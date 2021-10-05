a0aaa080b43e6e39e817964717c9a5eee3f8a3cd
dat nguyen <thedatnguyen20@gmail.com>
2021-10-05 15:53:08 +0700

    eureka

--- src/main/java/com/example/KhoaApplication.java
+++ src/main/java/com/example/KhoaApplication.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
+@EnableEurekaServer
 public class KhoaApplication {
 
 	public static void main(String[] args) {
